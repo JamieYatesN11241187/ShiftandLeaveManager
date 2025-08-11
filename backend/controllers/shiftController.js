b70f19c9f3aa4d82ca46a2c9d2075f3262e0daf2
JamieYatesN11241187 <132742618+JamieYatesN11241187@users.noreply.github.com>
2025-08-11 14:21:11 +1000

    Created backend connections to create shifts in the database - these are connected to the Roster.jsx

--- backend/controllers/shiftController.js
+++ backend/controllers/shiftController.js
@@ -1,6 +1,21 @@
 // controllers/shiftController.js
 const Shift = require('../models/Shift');
 
+exports.createShift = async (req, res) => {
+  try {
+    const { person, start, end } = req.body;
+    const shift = new Shift({
+      person: req.body.person,
+      start: req.body.start,
+      end: req.body.end,
+    });
+    await shift.save();
+    res.status(201).json(shift);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to create shift.', error: error.message });
+  }
+};
+
 exports.getShifts = async (req, res) => {
   try {
     const shifts = await Shift.find();
