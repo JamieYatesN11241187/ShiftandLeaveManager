49a101f87f72a0b2ee62cd3c71f91243fc4f4dde
JamieYatesN11241187 <132742618+JamieYatesN11241187@users.noreply.github.com>
2025-08-14 18:31:02 +1000

    Update axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.55.46.137:5001', // live
+  baseURL: '/api', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
