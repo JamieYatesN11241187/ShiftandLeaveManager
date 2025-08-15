b4bba4c356d6005fe473b143b8dfb6316945e857
JamieYatesN11241187 <132742618+JamieYatesN11241187@users.noreply.github.com>
2025-08-15 22:45:16 +1000

    Added navigation functionality to ensure a user, when logged in, is directly navigated to the roster page

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
+
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
